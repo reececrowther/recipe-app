6ac3be850b9d195c844cbab1c907f351e0db383a
reececrowther <reececrowther@hotmail.co.uk>
2022-08-22 21:57:20 +0100

    Filtering System using Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom"
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Recipe App</h1>
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import {Link, useParams} from 'react-router-dom'
+
+
+function Cuisine() {
+
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`);
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+    }
+
+    useEffect(() => {
+        getCuisine(params.type);
+        console.log(params.type);
+    }, [params.type]);
+
+  return (
+    <Grid>
+        {cuisine.map((item) => {
+            return(
+                <Card key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3em;
+`;
+
+const Card = styled.div`
+    img{
+        border-radius: 2em;
+        width: 100%;
+        height: 100%;
+    }
+    a{
+        text-decoration: none;
+    }
+    h4{
+        text-align: center;
+        padding: 1em;
+    }
+`;
+
+export default Cuisine
